c6c4102deb7c92b65f27ab0fbfa9d14d0d8b28cf
TalGurevich <tal.gurevich@gmail.com>
2018-04-01 20:45:54 +0300

    ds

--- KPI project/KPI_indexer.py
+++ KPI project/KPI_indexer.py
@@ -8,13 +8,13 @@ import time
 import numpy
 import os
 
-# opening unique companies with contract sync error file from running log_vs_contract.py file
+# contract sync error file from running log_vs_contract.py file
 with open('unique_contracts.csv', 'r') as contracts:
     error_contracts = 0
     for row in contracts:
         error_contracts = error_contracts +1
 
-# SQL find all spaceman accounts with contracts that month
+# SQL find all agreements on time period
 memberships = '''
 SELECT distinct count(uuid)
 from spaceman_public.membership_agreements ma
@@ -22,8 +22,6 @@ where ma.created_at > '2018-02-28'
 and ma.created_at < '2018-04-01'
 '''
 ma_ag = we.get_tbl_query(memberships)
-#divide this number by the length of unbique companies to get the KPI
-
 
 print('contracts created during this time:', ma_ag)
 print('sync errors during this time:', error_contracts)
