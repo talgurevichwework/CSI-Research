4673bfab6a2279bb83d42dd331d5c7f04933e88c
ymann1 <yonah.mann@wework.com>
2018-07-03 15:20:42 +0300

    test file

---
+++ Data reconciliation/test.py
@@ -0,0 +1,11 @@
+# from we_module.we import We
+import queries
+# we = We(True)
+# s = '\'2018-03-28\''
+# memberships = f''' SELECT * from spaceman_public.membership_agreements ma where ma.created_at > {s} and ma.created_at < '2018-04-01' '''
+# ma_ag = we.get_tbl_query(memberships)
+# print(memberships)
+time_period = 'Week'
+start_date = '2018-06-03'
+end_date = '2018-06-10'
+print(queries.create_looker_query(time_period, start_date, end_date))
