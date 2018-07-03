0cdd38b194125d8880522821b01879d71d9572b6
ymann1 <yonah.mann@wework.com>
2018-07-03 12:27:11 +0300

    script returns table of badly synced reservations. Renamed to main.py

--- Data reconciliation/main.py
+++ Data reconciliation/main.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
 import pandas as pd
 
 
-# In[6]:
+# In[7]:
 
 
 # Variables:
@@ -18,32 +18,32 @@ closed_lost_filename = 'cl.xlsx'
 looker_filename = '6:3-10 looker.csv'
 
 
-# In[12]:
+# In[8]:
 
 
 cw_df = pd.read_excel(closed_won_filename, sheet_name=0, header=11)
 
 
-# In[13]:
+# In[9]:
 
 
 cl_df = pd.read_excel(closed_lost_filename, sheet_name=0, header=13)
 
 
-# In[30]:
+# In[10]:
 
 
 looker_df = pd.read_csv(looker_filename, header=0)
 
 
-# In[15]:
+# In[11]:
 
 
 cw_df = cw_df.drop('Unnamed: 1', 1)
 cl_df = cl_df.drop('Unnamed: 1', 1)
 
 
-# In[16]:
+# In[12]:
 
 
 cl_df.drop(cl_df.columns[0:1],axis=1, inplace=True)
@@ -54,7 +54,7 @@ cw_df.drop(cw_df.columns[3:10], axis=1, inplace=True)
 cw_df.drop(cw_df.columns[4:8], axis=1, inplace=True)
 
 
-# In[26]:
+# In[14]:
 
 
 cw_df = cw_df[(cw_df['Close Date'] >= date_lower_bound) & (cw_df['Close Date'] < date_upper_bound)]
@@ -63,14 +63,14 @@ cw_df.drop('Close Date', 1)
 cl_df.drop('Close Date', 1).head()
 
 
-# In[27]:
+# In[15]:
 
 
 cl_df.groupby(['Account Name', 'UUID']).sum().sort_values(by='Total Desks Reserved (net)').reset_index()
 cw_df.groupby(['Account Name', 'UUID']).sum().sort_values(by='No. of Desks (unweighted)', ascending=False).reset_index().head()
 
 
-# In[28]:
+# In[79]:
 
 
 sf_df = cl_df.merge(cw_df, left_on=['UUID', 'Account Name'], right_on=['UUID', 'Account'                                                                                     ' Name'], how='outer')
@@ -80,7 +80,7 @@ sf_df['Net Desk Change'] = sf_df['Total Desks Reserved (net)'] + sf_df['No. of D
 sf_df.drop(sf_df.columns[[2,4]], axis=1, inplace=True)
 
 
-# In[33]:
+# In[80]:
 
 
 # looker_df = looker_df.drop(['Unnamed: 0', 'Sales Reporting Location Type', 'Sales Reporting Desk Upgrades', 'Sales'\
@@ -88,7 +88,7 @@ sf_df.drop(sf_df.columns[[2,4]], axis=1, inplace=True)
 #                             'Sales Reporting Total Desk Sales'], 1)
 
 
-# In[36]:
+# In[101]:
 
 
 comp_df = sf_df.merge(looker_df, how='outer', left_on='UUID', right_on='Accounts Account UUID')
@@ -96,20 +96,29 @@ comp_df['Net Desk Change']= comp_df['Net Desk Change'].fillna(0)
 comp_df['Sales Reporting Net Sales']= comp_df['Sales Reporting Net Sales'].fillna(0)
 
 
-# In[47]:
+# In[102]:
 
 
-comp_df['Absolute Desk Difference'] = abs(comp_df['Net Desk Change'] - comp_df['Sales Reporting Net Sales'])
+comp_df['Sf Looker Absolute Difference'] = abs(comp_df['Net Desk Change'] - comp_df['Sales Reporting Net Sales'])
 
 
-# In[49]:
+# In[103]:
 
 
-comp_df['Absolute Desk Difference'].sum()
+comp_df['Sf Looker Absolute Difference'].sum()
+comp_df['UUID'] = comp_df['UUID'].fillna(comp_df['Accounts Account UUID'])
 
 
-# In[52]:
+# In[104]:
 
 
-comp_df
+comp_df = comp_df[['Account Name', 'UUID', 'Close Date_x', 'Net Desk Change', 'Sales Reporting Net Sales',                    'Sf Looker Absolute Difference']]
+comp_df.rename(index=str, columns={"Close Date_x": "Close Date", "Net Desk Change": "SF Net Desk Change",                                  'Sales Reporting Net Sales':"Looker Net Desk Change"}, inplace=True)
+
+
+# In[106]:
+
+
+return_df = comp_df[comp_df['Sf Looker Absolute Difference'] != 0]
+return_df
 
