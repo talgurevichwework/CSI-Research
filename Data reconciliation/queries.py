1f228e21cdefcd37bc5bb7ea5edc16ed503ab3b7
Tal Gurevich <tal.gurevich@wework.com>
2018-07-10 17:44:35 +0300

    tt

--- Data reconciliation/queries.py
+++ Data reconciliation/queries.py
@@ -47,14 +47,17 @@ def create_spaceman_r_cr_ma_query(time_period, start_date, end_date):
 		cr as (
 			select *
 			from spaceman_public.change_requests cr)
-		select ma.account_uuid as account_uuid,
-			ma.uuid as ma_uuid,
-			cr.uuid as Change_uuid,
+		select cr.account_uuid as account_uuid,
+			ma.created_at as ma_created,
 			cr.created_at as cr_created,
-			r.uuid as reservation_uuid,
 			r.created_at as reservation_created,
-			cr.reservation_type,
-			cr.sf_opportunity_id  
+			r.uuid as reservation_uuid,
+			ma.uuid as ma_uuid,
+			cr.sf_opportunity_id,
+			ma.signed_at,
+			cr.reservation_started_on,
+			cr.reservation_ended_on,
+			r.description as reservable
 		from cr
 		join ma on ma.id = cr.membership_agreement_id
 		join r on r.id = cr.reservation_id
