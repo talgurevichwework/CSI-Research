c474c364bc9dcd401fed309b5710d77fd1b6c4a4
Tal Gurevich <tal.gurevich@wework.com>
2018-11-01 19:31:24 -0400

    Update Queries - Tal/zendesk ticket tagging.sql

--- Queries - Tal/zendesk ticket tagging.sql
+++ Queries - Tal/zendesk ticket tagging.sql
@@ -8,3 +8,13 @@ SELECT "tag",
 ticket_id
 FROM digital_zendesk.ticket_tag
 WHERE ticket_id = 131561;    – ticket_id
+
+
+--hours spent per tag
+SELECT "tag", count(tg.ticket_id), avg(EXTRACT(HOURS FROM (t.updated_at - t.created_at))) as diff
+FROM digital_zendesk.ticket_tag tg
+join digital_zendesk.ticket t on t.id = tg.ticket_id
+where t.created_at > '2018-01-01'
+and (lower("tag") like ('%enterprise%') or lower("tag") like '%manual%'
+group by "tag"
+order by count DESC
